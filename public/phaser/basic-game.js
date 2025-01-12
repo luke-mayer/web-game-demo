8623c4c1fa680142cfaaf22f26c8e373ebb581cb
luke-mayer <lemayer214@gmail.com>
2025-01-11 22:16:02 -0500

    making progress on learning how to create phaser stuff

--- public/phaser/basic-game.js
+++ public/phaser/basic-game.js
@@ -1,6 +1,13 @@
 class Example extends Phaser.Scene {
+  player;
+  cursors;
+
   constructor() {
-    super("MyFirstScene");
+    super("wg-demo");
+
+    this.player = null;
+    this.flashlight = null;
+    this.renderTexture = null;
   }
 
   preload() {
@@ -19,12 +26,24 @@ class Example extends Phaser.Scene {
         frameHeight: 48,
       },
     );
+
+    this.load.spritesheet("reticle", "./phaser/assets/IconsAndParticles.png", {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
   }
 
   create() {
-    this.add.image(400, 300, "sky");
+    const x = 400;
+    const y = 300;
 
-    this.player = this.add.sprite(400, 300, "handgunSprite");
+    // Background
+    this.add.image(x, y, "sky");
+
+    this.reticle = this.add.image(x, y, "reticle", 10);
+
+    this.player = this.physics.add.sprite(x, y, "handgunSprite");
+    this.player.setCollideWorldBounds(true);
 
     this.anims.create({
       key: "key",
@@ -34,6 +53,32 @@ class Example extends Phaser.Scene {
 
     this.cursors = this.input.keyboard.createCursorKeys();
 
+    // Set camera properties
+    this.cameras.main.zoom = 1.0;
+    this.cameras.main.startFollow(this.player);
+
+    // render texture for fog of war
+    const rt = this.make.renderTexture({
+      width: this.game.config.width,
+      height: this.game.config.height,
+      add: false,
+    });
+
+    // Fog of war
+    this.fow = this.add.graphics();
+    this.fow.fillStyle(0x000000, 1);
+    this.fow.fillRect(0, 0, this.game.config.width, this.game.config.height);
+
+    // Flashlight
+    this.flashlight = this.add.graphics();
+    const fowMask = this.flashlight.createGeometryMask();
+    this.fow.setMask(fowMask);
+
+    fowMask.invertAlpha = true;
+  }
+
+  update() {
+    // Movement with arrow keys
     if (this.cursors.left.isDown) {
       this.player.setVelocityX(-160);
       this.player.anims.play("key", true);
@@ -48,10 +93,47 @@ class Example extends Phaser.Scene {
     if (this.cursors.up.isDown) {
       this.player.setVelocityY(-160);
       this.player.anims.play("key", true);
-    } else if (this.cursors.up.isUp) {
+    } else if (this.cursors.down.isDown) {
       this.player.setVelocityY(160);
       this.player.anims.play("key", true);
+    } else {
+      this.player.setVelocityY(0);
     }
+
+    // Reticle follows mouse
+    this.input.on("pointermove", (pointer) => {
+      this.reticle.x = pointer.x;
+      this.reticle.y = pointer.y;
+    });
+
+    this.player.rotation = Phaser.Math.Angle.Between(
+      this.player.x,
+      this.player.y,
+      this.reticle.x,
+      this.reticle.y,
+    );
+
+    this.flashlight.clear();
+
+    const coneLength = 200;
+    const coneAngle = Phaser.Math.DegToRad(30);
+    const playerAngle = this.player.rotation;
+
+    const startX = this.player.x;
+    const startY = this.player.y;
+    const endX1 = startX + Math.cos(playerAngle - coneAngle) * coneLength;
+    const endY1 = startY + Math.sin(playerAngle - coneAngle) * coneLength;
+    const endX2 = startX + Math.cos(playerAngle + coneAngle) * coneLength;
+    const endY2 = startY + Math.sin(playerAngle + coneAngle) * coneLength;
+
+    this.flashlight.fillStyle(0xffffff, 1);
+    this.flashlight.beginPath();
+    this.flashlight.moveTo(startX, startY);
+    this.flashlight.lineTo(endX1, endY1);
+    this.flashlight.lineTo(endX2, endY2);
+    this.flashlight.closePath();
+    this.flashlight.fillPath();
+    this.light.visible = false;
   }
 }
 
@@ -59,7 +141,14 @@ const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
-  parent: "phaser-example",
+  //parent: "phaser-example",
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 0 },
+      debug: false,
+    },
+  },
   scene: Example,
 };
 
